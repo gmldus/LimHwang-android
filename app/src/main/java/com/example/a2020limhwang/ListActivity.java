5bc50fda5a69067db32dcb444be6cb57cdfa520a
iamkyunglim <bkyeonglim@gmail.com>
2020-01-18 18:43:42 +0900

    UI: 수업 목록 화면

---
+++ app/src/main/java/com/example/a2020limhwang/ListActivity.java
@@ -0,0 +1,16 @@
+package com.example.a2020limhwang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+
+
+}
