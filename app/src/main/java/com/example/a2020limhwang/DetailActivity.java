2e0124b6ef0992f53a4187a4331bdb2b427c9022
iamkyunglim <bkyeonglim@gmail.com>
2020-01-20 19:44:53 +0900

    UI:DetailActivity.java, font 디렉토리 추가

---
+++ app/src/main/java/com/example/a2020limhwang/DetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.a2020limhwang;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
